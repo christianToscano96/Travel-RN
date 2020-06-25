90694be4177b7fa0020ab59f8f5a4b88f5a0540f
chrisToscano96 <christoscano96@gmail.com>
2020-06-25 15:36:28 -0300

    top scrollview

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -17,6 +17,39 @@ const Home = () => {
   const image = {
     uri: 'https://injujuy.info/uploads/jujuy-turismo-interna-1.jpg',
   };
+
+  const [gallery, setgallery] = useState([
+    {
+      image: {
+        uri:
+          'https://www.visitingargentina.com/wp-content/uploads/2018/01/jujuy.jpg',
+      },
+      title: 'Tilcara',
+      key: '1',
+    },
+    {
+      image: {
+        uri:
+          'https://www.paginacentral.com.ar/wp-content/uploads/2018/04/Yungas.jpg',
+      },
+      title: 'Yungas',
+      key: '2',
+    },
+    {
+      image: {
+        uri:
+          'https://descubrirturismo.com/wp-content/uploads/2019/05/salinas-grandes-41-principal-11.jpg',
+      },
+      title: 'Salinas',
+      key: '3',
+    },
+    {
+      image: {uri: 'https://pbs.twimg.com/media/C9xoR6aXgAEqiwU.jpg'},
+      title: 'Ciudad De San Salvador',
+      key: '4',
+    },
+  ]);
+
   return (
     <View>
       <View>
@@ -50,6 +83,41 @@ const Home = () => {
             />
           </View>
         </ImageBackground>
+
+        <ScrollView>
+          <View style={{padding: 20}}>
+            <Text style={{fontSize: 22, fontWeight: 'bold'}}>Top Trending</Text>
+          </View>
+          <View style={{paddingVertical: 20, paddingLeft: 16}}>
+            <FlatList
+              data={gallery}
+              horizontal={true}
+              renderItem={({item}) => {
+                return (
+                  <TouchableOpacity>
+                    <Image
+                      source={item.image}
+                      style={{
+                        width: 150,
+                        height: 250,
+                        marginRight: 8,
+                        borderRadius: 10,
+                      }}
+                    />
+                    <View style={styles.imageOverlay} />
+                    <Icon
+                      name="heart"
+                      size={18}
+                      color="white"
+                      style={styles.imageLocationIcons}
+                    />
+                    <Text style={styles.imageText}>{item.title}</Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -108,6 +176,30 @@ const styles = StyleSheet.create({
     top: 30,
     right: 60,
     opacity: 0.6,
+  },
+  imageOverlay: {
+    width: 150,
+    height: 250,
+    marginRight: 8,
+    borderRadius: 10,
+    position: 'absolute',
+    backgroundColor: '#000',
+    opacity: 0.3,
+  },
+  imageLocationIcons: {
+    position: 'absolute',
+    marginTop: 4,
+    left: 10,
+    bottom: 10,
+  },
+  imageText: {
+    position: 'absolute',
+    color: 'white',
+    marginTop: 4,
+    fontSize: 18,
+    left: 30,
+    bottom: 10,
+    fontWeight: 'bold',
   },
 });
 
